d96161d1dbe01c1800dfea6700e7d5b60f7ea252
Laertes <maclellanj15@gmail.com>
2022-01-12 11:11:21 -0600

    working on for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,57 @@
 // variables
+var countdown = 50;
+var questionIdCounter = 0;
 var timerEl = document.getElementById('timer');
 var startEl = document.querySelector('#start-quiz');
 var pageContentEl = document.querySelector('#page-content');
 var questionsEl = document.querySelector("#questions");
+// question variables
+var questionOneObj = {
+    question: "Commonly used data types DO NOT include:",
+    answerOne: "1. strings",
+    answerTwo: "2. booleans",
+    answerThree: "3. alerts",
+    answerFour: "4. numbers"
+};
+
+var questionTwoObj = {
+    question: "The condition in an if/else statement is enclosed with _______.",
+    answerOne: "1. quotes",
+    answerTwo: "2. curly brackets",
+    answerThree: "3. parenthesis",
+    answerFour: "4. square brackets"
+};
+
+var questionThreeObj = {
+    question: "Arrays in Javascript can be used to store ______.",
+    answerOne: "1. numbers and strings",
+    answerTwo: "2. other arrays",
+    answerThree: "3. booleans",
+    answerFour: "4. all of the above"
+};
+
+var questionFourObj = {
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    answerOne: "1. commas",
+    answerTwo: "2. curly brackets",
+    answerThree: "3. quotes",
+    answerFour: "4. parenthesis"
+};
+
+var questionFiveObj = {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+    answerOne: "1. Javascript",
+    answerTwo: "2. terminal/bash",
+    answerThree: "3. for loops",
+    answerFour: "4. console log"
+};
+
+var questionsArr = [questionOneObj, questionTwoObj, questionThreeObj, questionFourObj, questionFiveObj];
 // localStorage array
 var highScores = [];
 
 // create first question
-var loadQuestionOne = function() {
+var loadQuestion = function() {
     // create new Question
     var questionEl = document.createElement("h2");
     var answersOneEl = document.createElement("button");
@@ -16,12 +60,20 @@ var loadQuestionOne = function() {
     var answersFourEl = document.createElement("button");
     // add class name for css here
 
+    // add question Id as attribute
+    questionEl.setAttribute("data-question-id", questionIdCounter);
+    answersOneEl.setAttribute("data-answers-id", questionIdCounter);
+    answersTwoEl.setAttribute("data-answers-id", questionIdCounter);
+    answersThreeEl.setAttribute("data-answers-id", questionIdCounter);
+    answersFourEl.setAttribute("data-answers-id", questionIdCounter);
+    
+
     // put in text
-    questionEl.textContent = "Commonly used data types DO NOT include:";
-    answersOneEl.textContent = "1. strings";
-    answersTwoEl.textContent = "2. booleans";
-    answersThreeEl.textContent = "3. alerts";
-    answersFourEl.textContent = "4. numbers";
+    questionEl.textContent = questionOneObj.question;
+    answersOneEl.textContent = questionOneObj.answerOne;
+    answersTwoEl.textContent = questionOneObj.answerTwo;
+    answersThreeEl.textContent = questionOneObj.answerThree;
+    answersFourEl.textContent = questionOneObj.answerFour;
     
     // append child to generate questions/answers
     questionsEl.appendChild(questionEl);
@@ -29,10 +81,19 @@ var loadQuestionOne = function() {
     questionsEl.appendChild(answersTwoEl);
     questionsEl.appendChild(answersThreeEl);
     questionsEl.appendChild(answersFourEl);
+
+    //
+    
 };
 
+// question cycle function
+var cycleQuestions = function() {
+    for (var i = 0; i < 5; i++) {
+        loadQuestion(questionsArr);
+        questionIdCounter++;
+    }
+}
+// event listeners
+startEl.addEventListener("click", cycleQuestions);
 
-startEl.addEventListener("click", loadQuestionOne);
-
-
-// console.log(startEl);
+console.log(questionsArr);
