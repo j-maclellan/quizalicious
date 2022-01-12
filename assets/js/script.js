908e3939113f17b8982b8749c2b1246111fd80d9
Laertes <maclellanj15@gmail.com>
2022-01-11 20:01:21 -0600

    first question function created

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,37 @@
 var timerEl = document.getElementById('timer');
 var startEl = document.querySelector('#start-quiz');
 var pageContentEl = document.querySelector('#page-content');
-
+var questionsEl = document.querySelector("#questions");
 // localStorage array
 var highScores = [];
 
-var loadQuestion = function() {
+// create first question
+var loadQuestionOne = function() {
     // create new Question
     var questionEl = document.createElement("h2");
-    questionEl.textContent = "Commonly used data types DO NOT include:"
+    var answersOneEl = document.createElement("button");
+    var answersTwoEl = document.createElement("button");
+    var answersThreeEl = document.createElement("button");
+    var answersFourEl = document.createElement("button");
+    // add class name for css here
 
-    var questionOneEl = document.querySelector("#question-one");
-    questionOneEl.appendChild(questionEl);
+    // put in text
+    questionEl.textContent = "Commonly used data types DO NOT include:";
+    answersOneEl.textContent = "1. strings";
+    answersTwoEl.textContent = "2. booleans";
+    answersThreeEl.textContent = "3. alerts";
+    answersFourEl.textContent = "4. numbers";
+    
+    // append child to generate questions/answers
+    questionsEl.appendChild(questionEl);
+    questionsEl.appendChild(answersOneEl);
+    questionsEl.appendChild(answersTwoEl);
+    questionsEl.appendChild(answersThreeEl);
+    questionsEl.appendChild(answersFourEl);
 };
 
-startEl.addEventListener("click", function() {
-    loadQuestion();
-});
+
+startEl.addEventListener("click", loadQuestionOne);
 
 
-console.log(startEl);
+// console.log(startEl);
